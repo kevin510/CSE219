b8a3d50c911386930f78a4755a886134e5df2c78
Kevin Gray <kevingray@130.245.72.187>
2018-01-29 11:19:05 -0500

    Added goodbye button + changed to flowpane

--- Recitation1/src/recitation1/Recitation1.java
+++ Recitation1/src/recitation1/Recitation1.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -22,7 +23,9 @@ public class Recitation1 extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
+        Button btn2 = new Button();
         btn.setText("Say 'Hello World'");
+        btn2.setText("Say 'Goodbye Cruel World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
@@ -30,9 +33,17 @@ public class Recitation1 extends Application {
                 System.out.println("Hello World!");
             }
         });
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Goodbye Cruel World!");
+            }
+        });
         
-        StackPane root = new StackPane();
+        FlowPane root = new FlowPane();
         root.getChildren().add(btn);
+        root.getChildren().add(btn2);
         
         Scene scene = new Scene(root, 300, 250);
         
