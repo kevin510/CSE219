5ef25fadd24a214cd64e1bf91e72074ee3ee02e5
Kevin Gray <kevingray@130.245.72.187>
2018-04-10 14:24:55 -0400

    applied css for toolbar buttons and chart

--- DataViliJ/data-vilij/src/ui/AppUI.java
+++ DataViliJ/data-vilij/src/ui/AppUI.java
@@ -1,19 +1,16 @@
 package ui;
 
 import actions.AppActions;
-import dataprocessors.AppData;
-import javafx.scene.control.CheckBox;
 import static java.io.File.separator;
 import java.io.IOException;
 import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
 import javafx.scene.Cursor;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import settings.AppPropertyTypes;
 import static settings.AppPropertyTypes.DATA_VILIJ_CSS_NAME;
@@ -38,12 +35,11 @@ public final class AppUI extends UITemplate {
 
     @SuppressWarnings("FieldCanBeLocal")
     private Button                       scrnshotButton; // toolbar button to take a screenshot of the data
-    private LineChart<Number, Number> chart;          // the chart where data will be displayed
+    private LineChart<Number, Number>    chart;          // the chart where data will be displayed
     private Button                       displayButton;  // workspace button to display data on the chart
     private TextArea                     textArea;       // text area for new data input
     private boolean                      hasNewText;     // whether or not the text area has any new data since last display
     private String scrnshotIconPath;
-    private CheckBox readChkBox;
     private String cssPathUI;
     
     public LineChart<Number, Number> getChart() { return chart; }
@@ -67,6 +63,7 @@ public final class AppUI extends UITemplate {
         super.setToolBar(applicationTemplate);
         scrnshotButton = setToolbarButton(scrnshotIconPath, applicationTemplate.manager.getPropertyValue(SCREENSHOT_TOOLTIP.name()), true);
         toolBar.getItems().add(scrnshotButton);
+        toolBar.getStylesheets().add(cssPath);
     }
 
     @Override
@@ -122,26 +119,27 @@ public final class AppUI extends UITemplate {
 
     private void layout() {
         PropertyManager manager = applicationTemplate.manager;
-        FlowPane mainPane = new FlowPane();
-        VBox sidePanel = new VBox();
+        GridPane mainPane = new GridPane();
         appPane.getChildren().add(mainPane);
-        appPane.getChildren().add(sidePanel);
         textArea = new TextArea();
-        readChkBox = new CheckBox(manager.getPropertyValue(AppPropertyTypes.CHECKBOX_TEXT.name()));
-        displayButton = new Button(manager.getPropertyValue(AppPropertyTypes.DISPLAY_BUTTON_TEXT.name()));
+        GridPane.setRowIndex(textArea, 0);
+        GridPane.setColumnIndex(textArea, 0);
+        textArea.setVisible(false);
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis(); 
         chart = new LineChart(xAxis, yAxis);
+        GridPane.setRowIndex(chart, 0);
+        GridPane.setColumnIndex(chart, 1);
         cssPathUI = "/" + String.join(separator,
                                              manager.getPropertyValue(GUI_RESOURCE_PATH.name()),
                                              manager.getPropertyValue(CSS_RESOURCE_PATH.name()),
                                              manager.getPropertyValue(DATA_VILIJ_CSS_NAME.name()));
-        chart.getStylesheets().add(cssPathUI);
+        mainPane.getStylesheets().add(cssPathUI);
+        //chart.getStyleClass().add("chart");
         chart.setHorizontalGridLinesVisible(false);
         chart.setVerticalGridLinesVisible(false);
         chart.setTitle(manager.getPropertyValue(AppPropertyTypes.CHART_TITLE.name()));
-        sidePanel.getChildren().addAll(displayButton, readChkBox);
-        mainPane.getChildren().addAll(textArea, sidePanel, chart);      
+        mainPane.getChildren().addAll(textArea, chart);
     }
     
     private void setWorkspaceActions() {
@@ -160,10 +158,6 @@ public final class AppUI extends UITemplate {
                 }
             }
         });
-                
-        readChkBox.setOnAction((ActionEvent e) -> {
-            textArea.setDisable(!textArea.isDisable());
-        });
         
         chart.setOnMouseEntered(e -> {
             chart.setCursor(Cursor.HAND);
@@ -172,15 +166,6 @@ public final class AppUI extends UITemplate {
         chart.setOnMouseExited(e -> {
             chart.setCursor(Cursor.DEFAULT);
         });
-        
-        displayButton.setOnAction((ActionEvent e) -> {
-            if(hasNewText == true) {
-                clearChart();
-                String plot = textArea.getText();                           
-                AppData dat = (AppData) applicationTemplate.getDataComponent();
-                dat.loadData(plot);
-                hasNewText = false;
-            }
-        });       
+              
     }
 }
